4a8f8916b5b29d9ef221596e699e90f2ad938c20
Muir-UCSB <82778280+Muir-UCSB@users.noreply.github.com>
2021-07-26 10:00:05 -0700

    Add files via upload

--- deOliviera_reproduction.py
+++ deOliviera_reproduction.py
@@ -60,31 +60,23 @@ if __name__ == "__main__":
     v2, ev = filter_ae(raw, filter, channel_num=2, sig_length=sig_len) # B1025
     v3, ev = filter_ae(raw, filter, channel_num=3, sig_length=sig_len) # B1025
 
-    channels = [v0, v1, v2, v3] # NOTE: set up waveform list to parse by channel
-
-
+    channels = [v0, v1, v2, v3] # NOTE: collect waveforms by channel
 
 
     '''
     Cast experiment as vectors
     '''
 
-    vect = []
+    feat_vect_set = []
     for i, channel in enumerate(channels):
         energy = energies[i]
         channel_vector = []
         for j, wave in enumerate(channel):
             feature_vector = extract_Moevus_vect(waveform=wave, energy=energy[j])
             channel_vector.append(feature_vector) # set of all waveforms from channel as a vector
-        vect.append(channel_vector) # set of all waveforms from experiment as vector index: i,j,k
+        feat_vect_set.append(channel_vector) # set of all feature vectors from experiment, index: i,j,k is (channel, feature vector, feature)
 
-    # NOTE: set of feature vectors by channel
-    ch0_X = vect[0]
-    ch1_X = vect[1]
-    ch2_X = vect[2]
-    ch3_X = vect[3]
 
-    feat_vect_set = [ch0_X, ch1_X, ch2_X, ch3_X]
 
 
     '''
@@ -93,30 +85,18 @@ if __name__ == "__main__":
 
 
     '''
-    Do PCA mapping on feature vectors by channel
+    No PCA
     '''
-    mapped_feature_vectors = []
-    pca = PCA(explained_var) #Note: determines the number of principal components to explain no less than 0.95 variance
 
-    for i, channel in enumerate(feat_vect_set):
-        X = pca.fit_transform(channel)
-        eigenvalues = pca.explained_variance_
-        mapped_feature_vectors.append(rescale(X, eigenvalues)) # NOTE: do rescaling of feature vectors so distances conform to metric in Moevus2008
 
-    ch0_X = mapped_feature_vectors[0]
-    ch1_X = mapped_feature_vectors[1]
-    ch2_X = mapped_feature_vectors[2]
-    ch3_X = mapped_feature_vectors[3]
 
-    dims = ch0_X.shape[1] # TODO: rename to ch0_dims
+
+    dims = np.array(feat_vect_set[0]).shape[1] # TODO: rename to ch0_dims
 
 
     # Initialization and training
     # NOTE: dataset has approx 200 data points, roccomendtiaons in 5*sprt(N), 8x8 is ok
     # NOTE: quantization error is average difference of output samples to winning neurons, https://www.intechopen.com/chapters/69305
-
-
-
 
     '''
     train SOM
@@ -124,8 +104,8 @@ if __name__ == "__main__":
 
     som = MiniSom(n_neurons, m_neurons, dims, sigma=sig, learning_rate=alpha,
               neighborhood_function='gaussian')
-    som.pca_weights_init(ch0_X)
-    som.train(ch0_X, max_som_iter, verbose=True)
+    som.pca_weights_init(feat_vect_set[0])
+    som.train(feat_vect_set[0], max_som_iter, verbose=True)
 
     ch0_weights = som.get_weights()
     ch0_weights = np.reshape(ch0_weights, (n_neurons*m_neurons, dims)) # NOTE: reshpae for kmeans
@@ -139,9 +119,9 @@ if __name__ == "__main__":
     kmeans_A = kmeans.fit(ch0_weights)
     A_lads = kmeans_A.labels_ # NOTE: grab labels
     A_lads = np.reshape(A_lads, (n_neurons, m_neurons))
-    ch0_labels = np.zeros(len(ch0_X), dtype=int)
+    ch0_labels = np.zeros(len(feat_vect_set[0]), dtype=int)
 
-    for i, feature_vect in enumerate(ch0_X): # NOTE: assign original feature vectors to the label that the closest weight vector channels
+    for i, feature_vect in enumerate(feat_vect_set[0]): # NOTE: assign original feature vectors to the label that the closest weight vector channels
         winner = som.winner(feature_vect) # NOTE: get winning neurons
         label = A_lads[winner[0]][winner[1]]# NOTE: get label of winning neurons
         ch0_labels[i]=label
