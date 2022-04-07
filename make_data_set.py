a683938eb0fec3f721eca9d7760c2b1964342a88
Muir-UCSB <82778280+Muir-UCSB@users.noreply.github.com>
2022-04-07 10:31:44 -0700

    Add files via upload

--- make_data_set.py
+++ make_data_set.py
@@ -7,13 +7,14 @@ import os
 from os import listdir, chdir
 from os.path import isfile, join
 import json
+import pandas as pd
 
 '''
 Script has very specific requirements. The data directory must ONLY contain
 waveform files and filter files. The waveform files must be named in the format
 date_angle_freeLeadLength_wave: XXXXXXX_XXdeg_Xmm_wave.txt. The filter files
-must be of the same format XXXXXXX_XXdeg_Xmm_filter.csv. There can be no other
-files in this directory.
+must be of the same format XXXXXXX_XXdeg_Xmm_filter.csv. The energy files must
+be same format _energy.txt. There can be no other files in this directory.
 '''
 
 
@@ -23,8 +24,8 @@ if __name__ == "__main__":
     Read-in and Setup
     '''
     sig_len = 1024
-    data_directory = 'E:/Research/Framework_Benchmarking/Data/220405_data_files/'
-    write_directory = 'E:/Research/Framework_Benchmarking/Data/'
+    data_directory = 'C:/Research/Framework_Benchmarking/Data/220405_data_files/'
+    write_directory = 'C:/Research/Framework_Benchmarking/Data/'
     write_to = 'PLB_data.json'
 
 
@@ -39,6 +40,7 @@ if __name__ == "__main__":
     labels = np.array([])
     target_angle = np.array([])
     target = np.array([])
+    energy_set = np.array([])
 
     angles = [] # NOTE: generate angle list, setup for parsing through file tree
     for i, file in enumerate(onlyfiles):
@@ -52,19 +54,28 @@ if __name__ == "__main__":
                 raw = file
             elif 'filter' in file:
                 filter = file
+            elif 'energy' in file:
+                energy_file = file
+                energy = pd.read_csv(energy_file,delimiter='\t')
+                energy = energy.to_numpy().T[1]
 
         v0, ev = filter_ae(raw, filter, channel_num=0)
+        energy = energy[ev-1]
         label = np.ones(len(v0))*i
 
         target = np.hstack((target,label)) # NOTE: true label of waveform i
+        energy_set = np.hstack((energy_set, energy))
         waves = np.hstack((waves, np.ravel(v0))) # NOTE: ravel needed for dimension agreement, reshape later
         # NOTE: CONTINUED: waves are put back into normal form later
 
+
     target = target.astype(int).tolist()
+    energy_set = energy_set.tolist()
     waves = waves.reshape(len(target), sig_len) # NOTE: don't flip order of
     waves = waves.tolist()
 
-    PLB_data = {'data':waves, 'target':target, 'target_angle':target_angle}
+    PLB_data = {'data':waves, 'target':target, 'target_angle':target_angle,
+        'energy':energy_set}
 
 
 
